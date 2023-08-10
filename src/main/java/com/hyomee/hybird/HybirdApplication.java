19f00626e988aeb602d0bc830f7aeb7f912171e3
hyosang <hyomee@naver.com>
2023-08-11 00:45:40 +0900

    최초 커밋

---
+++ src/main/java/com/hyomee/hybird/HybirdApplication.java
@@ -0,0 +1,13 @@
+package com.hyomee.hybird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybirdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HybirdApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hyomee/hybird/HybirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyomee.hybird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybirdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
